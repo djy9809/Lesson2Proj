ae89652c36097f7e61872d9258d53d02d739da5c
djy9809 <357868050@qq.com>
2018-06-09 15:00:45 +0800

    1、完善某些代码

--- app/src/main/java/com/example/djy/lesson2proj/MainActivity.java
+++ app/src/main/java/com/example/djy/lesson2proj/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.djy.lesson2proj;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;//先声明
+    private EditText et_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.center);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is a text toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
